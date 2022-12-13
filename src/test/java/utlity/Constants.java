ab94009c6244521f85028f63d094d7cf43ec754e
SugunaRagavareddy <rsuguna92@gmail.com>
2022-12-13 18:14:28 +0530

    commit to main
    add all automation code

---
+++ src/test/java/utlity/Constants.java
@@ -0,0 +1,12 @@
+package utlity;
+
+public class Constants {
+
+	
+	public static final String URL = "https://demoqa.com/automation-practice-form";
+    public static final String Path_TestData = ".\\testData\\";
+    public static final String File_TestData = "RegisterStudent.xls";
+    public static final String File_TestData2 = "RegistrationData.xlsx";
+
+    
+}
